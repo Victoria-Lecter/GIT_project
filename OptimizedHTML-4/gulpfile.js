ac1cf7c4435428470f282815a795e8f599fa550d
Victoria-Lecter <victoria.doshchenko@gmail.com>
2018-09-16 20:57:05 +0200

    сделала dropdown юзера в шапке, половину страниц в студенте, почти разобралась с inputs

--- OptimizedHTML-4/gulpfile.js
+++ OptimizedHTML-4/gulpfile.js
@@ -37,7 +37,8 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		// 'app/libs/uikit/src/js/uikit-core.js',
+		'app/libs/uikit/dist/js/uikit.js',
+		'app/libs/uikit/dist/js/uikit-icons.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
